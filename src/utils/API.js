e7610aa6ad264aabe218d8f5b1e4d34e4cec1063
Donald Wu <chenggeuw0512@gmail.com>
2021-05-03 18:15:40 +0930

    API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-//adding in user generator link Forrest sent
 const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
 
-
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: function(query) {
+  search: function (query) {
     return axios.get(BASEURL);
-  }
+  },
 };
